b6af8cf641b9faacc3154117a5c3b106c00ac2e4
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-06 15:25:08 -0500

    added logout and login hooks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,14 @@
 // Use MUI Later
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const NavBar = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
     return(
         <header>
             <div className="container">
@@ -9,6 +16,9 @@ const NavBar = () => {
                     <h1>My Workout Buddy</h1>
                 </Link>
                 <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
                     <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
