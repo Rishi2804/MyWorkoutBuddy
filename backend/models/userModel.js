951ba27826d920f80ee0fd3fcf6f831e90f7dadc
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-06 00:19:08 -0500

    added signing tokens

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,6 +36,7 @@ userSchema.statics.signup = async function(email, password) {
         throw Error('Email already in use')
     }
 
+    // encrypy password
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
