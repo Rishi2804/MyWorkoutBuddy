8be6d4124e48fb4d771c455e42344e2ae49cb95e
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-05 23:59:14 -0500

    added email validation and password strength detector

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,7 +18,20 @@ const userSchema = new Schema({
 
 // static singup method
 userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must exist')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
     const exists = await this.findOne({ email })
+
     if (exists) {
         throw Error('Email already in use')
     }
