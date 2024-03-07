4e977c97a6de305a241add84c19389994338fdfa
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-07 13:40:36 -0500

    changed and updated workout schem as well as post controllera

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-
-
-const workoutSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
+const setSchema = new Schema({
     reps: {
         type: Number,
         required: true
@@ -16,11 +10,42 @@ const workoutSchema = new Schema({
     weight: {
         type: Number,
         required: true
+    }
+}, { _id: false })
+
+const exerciseSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    sets: {
+        type: [setSchema],
+        required: true
+    }
+}, { _id: false })
+
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    exercises: {
+        type: [exerciseSchema],
+        required: true
     },
     user_id: {
         type: String,
         required: true
     }
-}, { timestamps : true })
+})
 
 module.exports = mongoose.model('Workout', workoutSchema)
