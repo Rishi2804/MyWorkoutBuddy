3d104b8b922c3095c3e6058ed9bdbba4554841f0
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-07 23:32:50 -0500

    started history page workout previews

--- frontend/src/pages/History/History.js
+++ frontend/src/pages/History/History.js
@@ -1,6 +1,47 @@
+import './History.css'
+import { useEffect } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useWorkoutsContext } from '../../hooks/UseWorkoutsContext'
+
+// components
+import WorkoutView from '../../components/WorkoutView/WorkoutView'
 
 const History = () => {
-    return <h3>History</h3>
+    const { workouts, dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        if (user) {
+            fetchWorkouts()
+        }
+    }, [dispatch, user])
+
+
+    return (
+        <div>
+            <h2>History</h2>
+            <div className='workout-history'>
+                {
+                    workouts && workouts.map((w) => (
+                        <WorkoutView key={w._id} workout={w} />
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default History
