7732d2a911c088bcc368607197223bc33caa278d
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-03-10 00:53:58 -0500

    Added exercise modal

--- frontend/src/components/ExerciseView/ExerciseView.js
+++ frontend/src/components/ExerciseView/ExerciseView.js
@@ -1,40 +1,16 @@
-import { Paper } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import ExcerciseModal from '../ExerciseModal/ExcerciseModal'
 
 const ExerciseView = ({ exercise }) => {
 
-    const EnclosureCard = styled(Paper)(() => ({
-        display: 'flex',
-        flexDirection: 'column',
-        width: '17vw',
-        padding: '5px',
-        margin: '5px 0',
-        marginRight: '1.2em',
-        backgroundColor: 'white',
-        borderRadius: '25px',
-        position: 'relative',
-        overflow: 'hidden',
-        '& h3': {
-            whiteSpace: 'nowrap', // Prevent wrapping
-            overflow: 'hidden', // Hide overflow
-            textOverflow: 'ellipsis' // Show ellipsis for overflow
-        },
-        '&:hover': {
-            '& h3': {
-                color: '#58a5f8'
-            },
-        },
-    }));
-
     return (
-        <EnclosureCard>
-            <img src={exercise.images[0] + "?tr=w-250,h-180"} alt={exercise.id}/>
+        <ExcerciseModal exercise={exercise}>
+            <img src={exercise.images[0] + "?tr=w-250,h-180"} alt={exercise.id} />
             <h3>{exercise.name}</h3>
             <div className="details">
                 <span>{exercise.category}</span>
                 <span>{exercise.level}</span>
             </div>
-        </EnclosureCard>
+        </ExcerciseModal>
     )
 }
 
